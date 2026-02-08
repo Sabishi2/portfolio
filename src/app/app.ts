f9944eca9b49a09e78e7f6ff40a04dc2cfc57226
Sabishi2 <vehamaki@gmail.com>
2026-02-08 13:45:26 +0200

    Projects now import from json, assets folder added

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,53 +4,9 @@ import { ProjectIcon } from './projects/project-icons/project-icon';
 import { ProjectT } from './projects/project-type';
 import { ProjectPage } from './projects/project-pages/project-page';
 import { PICarouselHolderModule } from './projects/project-icons/pi-carousel/pi-carousel-holder-module';
+import projects_json from "../assets/projects.json"
 
-const p1: ProjectT = {
-  id: 1,
-  name: "Ao Onidle",
-  shortDescription: "Ao Oni guessing game website",
-  longDescription: "Longer description",
-  imageURL: "logo_real.png",
-  public: true,
-  linkToProject: "#"
-}
-const p2: ProjectT = {
-  id: 2,
-  name: "bileet",
-  shortDescription: "Ao Oni guessing game website",
-  longDescription: "Longer description",
-  imageURL: "Ao_Onidle/1.png",
-  public: true,
-  linkToProject: "#"
-}
-const p3: ProjectT = {
-  id: 3,
-  name: "XD",
-  shortDescription: "Ao Oni guessing game website",
-  longDescription: "Longer description",
-  imageURL: "logo_real.png",
-  public: true,
-  linkToProject: "#"
-}
-const p4: ProjectT = {
-  id: 4,
-  name: "Joo",
-  shortDescription: "Ao Oni guessing game website",
-  longDescription: "Longer description",
-  imageURL: "logo_real.png",
-  public: true,
-  linkToProject: "#"
-}
-const p5: ProjectT = {
-  id: 5,
-  name: "Juu",
-  shortDescription: "Ao Oni guessing game website",
-  longDescription: "Longer description",
-  imageURL: "Ao_Onidle/1.png",
-  public: true,
-  linkToProject: "#"
-}
-const projects: Array<ProjectT> = [p1, p2, p3, p4, p5];
+const projects: Array<ProjectT> = projects_json["projects"];
 
 @Component({
   selector: 'app-root',
