44845817df3c60f9fcd146d006bdcd53331af64e
Sabishi2 <vehamaki@gmail.com>
2026-02-08 16:38:16 +0200

    Project pages dispalys links and alternate links now, added link parsing function

--- src/app/projects/project-pages/project-page.ts
+++ src/app/projects/project-pages/project-page.ts
@@ -2,6 +2,7 @@ import { Component, input } from '@angular/core';
 import { ProjectT } from '../project-type';
 import { PPCarouselHolderModule } from './pp-carousel/pp-carousel-holder-module';
 
+
 @Component({
     imports: [PPCarouselHolderModule],
     selector: 'project-page',
@@ -10,6 +11,37 @@ import { PPCarouselHolderModule } from './pp-carousel/pp-carousel-holder-module'
 })
 
 
+
 export class ProjectPage {
+    isUndefined(param1: any) {
+        return param1 === undefined;
+    }
+    parseLinkText(linkText: string, linkToProject: string) {
+        // (?:[^.\\][\[]) -> Match '[' character that is not preceded with '\'
+        // (?:[^.\\][\]]) -> Match ']' character that is not preceded with '\'
+
+        let fullText: string;
+        const linkStart: number = linkText.search(/(?:[^.\\][\[])/gm) + 1; // Index the actual opening bracket(not the character before it)
+        if (linkStart <= 0) {
+            // If there is no brackets
+            fullText = linkText + ` <a href="${linkToProject}">link</a>`;
+        }
+        else {
+            const linkEnd: number = linkText.search(/(?:[^.\\][\]])/gm) + 1; // Same with end bracket
+            const fullTextStart: string = linkText.substring(0, linkStart);
+            let fullTextMiddle: string = linkText.substring(linkStart + 1, linkEnd); // Omit the '[' and ']'
+            const fullTextEnd: string = linkText.substring(linkEnd + 1);
+            console.log(fullTextEnd);
+            fullTextMiddle = `<a href="${linkToProject}">${fullTextMiddle}</a>`
+            fullText = fullTextStart + fullTextMiddle + fullTextEnd;
+        }
+
+        // /(?<!(?:[^\\][\\])+(?:[\\]{2})*)\\/gm -> 
+        // Match escape character '\' that is not followed by 2x escape char '\\' 
+        // and any char except escape char + escape char '(not \)\'
+        fullText = fullText.replaceAll(/(?<!(?:[^\\][\\])+(?:[\\]{2})*)\\/gm, "");
+        return fullText
+
+    }
     project = input.required<ProjectT>();
 }
--- src/app/projects/project-type.ts
+++ src/app/projects/project-type.ts
@@ -1,17 +1,21 @@
 export type ProjectT = {
-    id: number,
-    name: string;
-    imageURL: string;
-    screenshotFolder?: string;
-    screenshotURLs?: Array<string>;
-    longDescription: string;
-    shortDescription: string;
-    public: boolean;
+    id: number, // done
+    name: string; // done
+    imageURL: string; // done
+    screenshotFolder?: string; // done
+    screenshotURLs?: Array<string>; // done
+    longDescription: string; // done
+    shortDescription: string; // done
+    public: boolean; // done
     linkToProject: string;
+    alternateLinkToProject?: string;
+    alternateLinkText?: string;
+    // for linkText wrap the section you want to hyperlink with square brackets: []  
+    // Alternatively the link will be pasted in the end with [link]
+    // To use square brackets in your text, use escape character: \
+    // The JSON file will need an escape character for the escape character so for example: \\[
     linkText?: string;
     demoYouTubeLink?: string;
     technologiesUsed?: Array<string>;
-    alternateLinkToProject?: string;
-    alternateLinkText?: string;
 }
 
