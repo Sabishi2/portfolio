2ce4cad45ecd9293ca2ece0dccb83dae1273e9a7
Sabishi2 <vehamaki@gmail.com>
2026-03-18 19:38:33 +0200

    Made changes to front page to show certificates page, added certificate template file

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,15 +3,15 @@ import { RouterOutlet, RouterModule } from '@angular/router';
 import { ProjectT } from './projects/project-type';
 import { ProjectPage } from './projects/project-pages/project-page';
 import { PICarouselHolderModule } from './projects/project-icons/pi-carousel/pi-carousel-holder-module';
-import projects_json from "../assets/projects.json"
-import { SetionNav } from './section-navigation/section-navigation';
+import projects_json from "../assets/projects.json";
+import { SectionNav } from "./section-navigation/section-navigation";
 import { NavButton } from './section-navigation/nav-button/nav-button';
 
 const projects: Array<ProjectT> = projects_json["projects"];
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProjectPage, RouterModule, PICarouselHolderModule, SetionNav, NavButton],
+  imports: [RouterOutlet, ProjectPage, RouterModule, PICarouselHolderModule, SectionNav, NavButton],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- src/app/section-navigation/section-navigation.ts
+++ src/app/section-navigation/section-navigation.ts
@@ -1,10 +1,10 @@
-import { Component, input } from '@angular/core';
+import { Component } from '@angular/core';
 @Component({
     selector: 'section-navigation',
     templateUrl: './section-navigation.html',
     styleUrl: './section-navigation.scss',
 })
 
-export class SetionNav {
+export class SectionNav {
 
 }
